ea84d977338c29ede33459f9407ad413e4f40491
psharma316 <67465435+psharma316@users.noreply.github.com>
2020-08-07 13:20:45 +0530

    Add files via upload

---
+++ git_clone.sh
@@ -0,0 +1,3 @@
+gcloud source repos clone github_psharma316_test_rop1 --project=dataproc-usecase-276411
+cd github_psharma316_test_rop1
+gsutil cp /home/bdondataproc/github_psharma316_test_rop1/automation.py gs://dphivedb/
